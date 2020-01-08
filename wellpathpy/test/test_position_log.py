ab1b670c7f161d74789a01bc3978a0a961e9cb7d
Jørgen Kvalsvik <jokva@equinor.com>
2020-01-08 22:44:18 +0100

    Add deviation and position_log objects
    The new interface for deviation and position logs abstract away the
    array details, and help callers focus on the mapping between log types.
    There's often a lot of non-trivial book keeping involved in converting
    between formats, so an object that maintains this state is very helpful.
    
    The usability is a net win, although only min-curve support is added for
    now. The position log -> deviation is missing and will always raise
    NotImplemented for now, as it is probably method dependent, and seems
    quite hard to do accurately.
    
    The idea and implementation is largely inspired by David M. Baker's
    arc2chord.

---
+++ wellpathpy/test/test_position_log.py
@@ -0,0 +1,42 @@
+from hypothesis import assume
+from hypothesis import given
+from hypothesis.strategies import composite
+import numpy as np
+
+from . import same_len_lists
+from .. import deviation
+
+@composite
+def deviation_survey(draw):
+    dev = draw(same_len_lists(min_value = 0))
+    md  = np.unique(dev[0])
+    inc = np.mod(dev[1], 90)[:len(md)]
+    azi = np.mod(dev[2], 360)[:len(md)]
+    assume(len(md) >= 2)
+    return md, inc, azi
+
+@given(deviation_survey())
+def test_resample_onto_unchanged_md(survey):
+    md, inc, azi = survey
+
+    dev = deviation(md, inc, azi)
+    pos = dev.minimum_curvature()
+    resampled = pos.resample(depths = md)
+
+    np.testing.assert_allclose(pos.tvd, resampled.tvd)
+    np.testing.assert_allclose(pos.northing, resampled.northing)
+    np.testing.assert_allclose(pos.easting, resampled.easting)
+
+@given(deviation_survey())
+def test_resample_onto_unchanged_md_unchanged_deviation(survey):
+    md, inc, azi = survey
+
+    dev = deviation(md, inc, azi)
+    pos = dev.minimum_curvature()
+    resampled = pos.resample(depths = md)
+
+    dev2 = resampled.deviation()
+
+    np.testing.assert_allclose(dev.md, dev2.md)
+    np.testing.assert_allclose(dev.inc, dev2.inc)
+    np.testing.assert_allclose(dev.azi, dev2.azi)
